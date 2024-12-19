d5fbb3ec4d70e9cd8663d9533e1174b0469b6fa6
Henrique <henriquemoreira10fk@gmail.com>
2024-12-19 17:15:34 -0300

    Added working CLI

--- turbodl/__init__.py
+++ turbodl/__init__.py
@@ -3,7 +3,7 @@ from typing import List
 
 # Local imports
 from .downloader import TurboDL
-from .exceptions import DownloadError, RequestError
+from .exceptions import DownloadError, RequestError, TurboDLError
 
 
-__all__: List[str] = ['TurboDL', 'DownloadError', 'RequestError']
+__all__: List[str] = ['TurboDL', 'DownloadError', 'RequestError', 'TurboDLError']
