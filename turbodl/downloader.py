a41d52f06a937c02864d3f1844da5a32497e9810
Henrique <henriquemoreira10fk@gmail.com>
2024-12-26 17:44:01 -0300

    Moved ChunkBuffer to main code

--- turbodl/downloader.py
+++ turbodl/downloader.py
@@ -2,6 +2,7 @@
 from concurrent.futures import ThreadPoolExecutor
 from functools import lru_cache
 from hashlib import new as hashlib_new
+from io import BytesIO
 from math import ceil, log2, sqrt
 from mimetypes import guess_extension as guess_mimetype_extension
 from mmap import mmap
@@ -14,12 +15,68 @@ from urllib.parse import unquote, urlparse
 
 # Third-party imports
 from httpx import Client, HTTPStatusError, Limits
+from psutil import virtual_memory
 from rich.progress import BarColumn, DownloadColumn, Progress, SpinnerColumn, TextColumn, TimeRemainingColumn, TransferSpeedColumn
 from tenacity import retry, stop_after_attempt, wait_exponential
 
 # Local imports
 from .exceptions import DownloadError, HashVerificationError, InsufficientSpaceError, RequestError
-from .utils import ChunkBuffer
+
+
+class ChunkBuffer:
+    """
+    A class for buffering chunks of data.
+    """
+
+    def __init__(self, chunk_size_mb: int = 128, max_buffer_mb: int = 2048) -> None:
+        """
+        Initialize the ChunkBuffer class.
+
+        Args:
+            chunk_size_mb: The size of each chunk in megabytes.
+            max_buffer_mb: The maximum size of the buffer in megabytes.
+        """
+
+        self.chunk_size = chunk_size_mb * 1024 * 1024
+        self.max_buffer_size = min(max_buffer_mb * 1024 * 1024, virtual_memory().available * 0.25)
+        self.current_buffer = BytesIO()
+        self.current_size = 0
+        self.total_buffered = 0
+
+    def write(self, data: bytes, total_file_size: int) -> Optional[bytes]:
+        """
+        Write data to the buffer.
+
+        Args:
+            data: The data to write to the buffer.
+            total_file_size: The total size of the file in bytes.
+
+        Returns:
+            The chunk data if the buffer is full, None otherwise.
+        """
+
+        self.current_buffer.write(data)
+        self.current_size += len(data)
+        self.total_buffered += len(data)
+
+        if total_file_size <= self.max_buffer_size:
+            if self.total_buffered >= total_file_size:
+                chunk_data = self.current_buffer.getvalue()
+                self.current_buffer = BytesIO()
+                self.current_size = 0
+
+                return chunk_data
+
+            return None
+
+        if self.current_size >= self.chunk_size:
+            chunk_data = self.current_buffer.getvalue()
+            self.current_buffer = BytesIO()
+            self.current_size = 0
+
+            return chunk_data
+
+        return None
 
 
 class TurboDL:
