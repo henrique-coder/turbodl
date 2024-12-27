e0ef3529837168f49b710ee03e6bfe1819bc4d13
Henrique <henriquemoreira10fk@gmail.com>
2024-12-27 07:42:49 -0300

    Removed bad request header

--- turbodl/downloader.py
+++ turbodl/downloader.py
@@ -134,7 +134,6 @@ class TurboDL:
             self._custom_headers: Dict[Any, Any] = {
                 "Accept": "*/*",
                 "Accept-Encoding": "identity",
-                "Connection": "keep-alive",
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
             }
 
