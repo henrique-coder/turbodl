0cd09bf9cbb663294f5cebf8e620bd230ce38526
Henrique <henriquemoreira10fk@gmail.com>
2025-02-17 14:22:09 -0300

    update: user agent version and some constants

--- turbodl/constants.py
+++ turbodl/constants.py
@@ -3,14 +3,14 @@ from typing import Final, Literal
 
 
 # Size constants
-ONE_MB: Final[int] = 1048576
-ONE_GB: Final[int] = 1073741824
+ONE_MB: Final[int] = 1024**2
+ONE_GB: Final[int] = 1024**3
 
 # Chunk size constants
-MIN_CHUNK_SIZE: Final[int] = 16777216  # 16 MB
-MAX_CHUNK_SIZE: Final[int] = 268435456  # 256 MB
-CHUNK_SIZE: Final[int] = 268435456  # 256 MB
-MAX_BUFFER_SIZE: Final[int] = 2147483648  # 2 GB
+MIN_CHUNK_SIZE: Final[int] = 16 * 1024**2
+MAX_CHUNK_SIZE: Final[int] = 256 * 1024**2
+CHUNK_SIZE: Final[int] = 256 * 1024**2
+MAX_BUFFER_SIZE: Final[int] = 2 * 1024**3
 
 # Connection constants
 MAX_CONNECTIONS: Final[int] = 24
@@ -22,9 +22,9 @@ RAM_FILESYSTEMS: Final[frozenset[str]] = frozenset({"tmpfs", "ramfs", "devtmpfs"
 # HTTP headers
 DEFAULT_HEADERS: Final[tuple[dict[str, str], ...]] = (
     {"Accept": "*/*"},
-    {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"},
+    {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"},
 )
 REQUIRED_HEADERS: Final[tuple[dict[str, str], ...]] = ({"Accept-Encoding": "identity"},)
 
 # Units and values
-YES_NO_VALUES: Final[tuple[Literal["no"], Literal["yes"]]] = ("no", "yes")  # "no", "yes"
+YES_NO_VALUES: Final[tuple[Literal["no"], Literal["yes"]]] = ("no", "yes")
