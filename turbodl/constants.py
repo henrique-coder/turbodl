2acc2392bb245ad8d0951c7bf9952980fbf39515
Henrique <henriquemoreira10fk@gmail.com>
2025-03-16 21:01:08 -0300

    feat: update default HTTP headers for enhanced content negotiation and security

--- turbodl/constants.py
+++ turbodl/constants.py
@@ -21,11 +21,24 @@ RAM_FILESYSTEMS: Final[frozenset[str]] = frozenset({"tmpfs", "ramfs", "devtmpfs"
 
 # HTTP headers
 DEFAULT_HEADERS: Final[tuple[dict[str, str], ...]] = (
-    {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"},
-    {"Accept-Language": "en-US,en;q=0.5"},
+    {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
+    },
     {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
     },
+    {"DNT": "1"},
+    {"Connection": "keep-alive"},
+    {"Cache-Control": "max-age=0"},
+    {"Accept-Language": "en-US,en;q=0.9"},
+    {"Sec-Fetch-Dest": "document"},
+    {"Sec-Fetch-Mode": "navigate"},
+    {"Sec-Fetch-Site": "none"},
+    {"Sec-Fetch-User": "?1"},
+    {"Upgrade-Insecure-Requests": "1"},
+    {"sec-ch-ua": '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"'},
+    {"sec-ch-ua-mobile": "?0"},
+    {"sec-ch-ua-platform": '"Windows"'},
 )
 REQUIRED_HEADERS: Final[tuple[dict[str, str], ...]] = ({"Accept-Encoding": "identity"},)
 
